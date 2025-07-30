36011c81fe25730802f7b23dc563969278d25798
Muhammad Dausyaf Aryandha <muhammaddausyaf@gmail.com>
2025-07-30 11:51:39 +0700

    delete chart and all

--- pages/index.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ padding: '20px' }}>
-      <h1>Selamat Datang di Portofolio Saya</h1>
-      <p>Nama saya [Nama Anda], developer dengan pengalaman Laravel dan Next.js.</p>
-      <p>Proyek: Aplikasi Todo, Crypto Trends Visualizer.</p>
-    </div>
-  );
-}
--- pages/trends.tsx
+++
@@ -1,8 +0,0 @@
-export default function Trends() {
-  return (
-    <div style={{ padding: '20px' }}>
-      <h1>Tren Harga Crypto</h1>
-      <p>Grafik akan ditampilkan di sini nanti.</p>
-    </div>
-  );
-}
